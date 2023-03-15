d8470022392a165f8087f42f5a08d0bd1b05467f
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-03-15 13:52:14 -0400

    last&

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
-import css from '../index.css';
+import css from './App.module.css';
+
 
 
 export const App = () => {
  return (
-    <div className="thumb">
+    <div className={css.thumb}>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
